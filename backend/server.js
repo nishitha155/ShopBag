11d544eb8d66bb1a80d0e6b3f31e8a00b951e0c8
Lakshmi Nishitha Charugundla <charugundlalakshminishitha@gmail.com>
2024-08-18 22:05:46 +0530

    vercel

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 // const { mailTransport, generateEmailTemplate, plainEmailTemplate } = require('./utils/mail');
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://shop-bag--psi.vercel.app',
   credentials: true,
 };
 app.use(cors(corsOptions));
